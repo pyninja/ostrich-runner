7d9e5c179ce83303d9f713bcdd87377354cf2b86
pyninja <thepyninja@gmail.com>
2013-01-31 02:33:17 +0100

    Move a RecordNameInput to center.

--- launcher.py
+++ launcher.py
@@ -2,10 +2,11 @@ import sys
 
 from PySide.QtGui import QApplication
 
-from ostrich_runner.ui import RecordNameInput
+from ostrich_runner.ui import *
 
 if __name__ == '__main__':
     application = QApplication(sys.argv)
     input = RecordNameInput()
     input.show()
+    on_desktop_center(input)
     application.exec_()
--- ostrich_runner/ui.py
+++ ostrich_runner/ui.py
@@ -2,8 +2,16 @@ from PySide.QtCore import *
 from PySide.QtGui import *
 from PySide import QtGui
 
+from ostrich_runner.record_tools import *
+
 
 class RecordNameInput(QLineEdit, object):
+
+    def __init__(self, *args, **kwargs):
+        super(RecordNameInput, self).__init__(*args, **kwargs)
+        record_names = get_record_names()
+        self.set_tips(record_names)
+        self.setWindowTitle(u'OstrichRunner')
 
     def set_tips(self, record_names):
         self.setCompleter(QCompleter(record_names))
@@ -19,3 +27,10 @@ class RecordNameInput(QLineEdit, object):
             QtGui.qApp.quit()
         else:
             super(RecordNameInput, self).keyPressEvent(event)
+
+
+def on_desktop_center(window):
+    frame = window.frameGeometry()
+    center = QDesktopWidget().availableGeometry().center()
+    frame.moveCenter(center)
+    window.move(frame.topLeft())
